a4a2f364287462ec7b510a7ba7ccfc2a15cac7fe
thelastbrittain <benjaminpbrittain@gmail.com>
2025-02-01 10:33:41 -0700

    Write all authRouter Tests -- 55.36% total line coverage

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,8 +1,16 @@
 const request = require('supertest');
 const app = require('../service');
 
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -21,6 +29,35 @@ test('login', async () => {
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
-function expectValidJwt(potentialJwt) {
-  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+async function register() {
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(registerRes.body.user).toMatchObject(expectedUser);
+  let token = registerRes.body.token;
+  let id = registerRes.body.user.id;
+
+  return { token, id };
+
 }
+
+test('register', register);
+
+test ('updateUser', async () => {
+  const { token, id } = await register();
+  const updatedUser = { ...testUser, email: 'new' + testUser.email };
+  const updateUserRes = await request(app).put(`/api/auth/${id}`).set('Authorization', `Bearer ${token}`).send(updatedUser);
+  expect(updateUserRes.status).toBe(200);
+  expect(updateUserRes.body.email).toBe(updatedUser.email);
+});
+
+test ('logout', async () => {
+  const {token} = await register();
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${token}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+});
